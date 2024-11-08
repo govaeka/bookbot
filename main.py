8b4896e30436c3420d865925502c512a625cd1d2
kago <karen.govaerts@gmail.com>
2024-11-08 10:26:31 +0100

    added character count function

--- main.py
+++ main.py
@@ -4,19 +4,28 @@ def main():
 	#print(text)
 	number_of_words = count_words(text)
 	print(number_of_words)
+	letter_count = count_characters(text)
+	print(letter_count)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 
-def count_words(text):
-	list_of_words = text.split()
+def count_words(tw):
+	list_of_words = tw.split()
 	return len(list_of_words)
 
 
-#77986 
-
+def count_characters(tc):
+	tc = tc.lower()
+	ledger = {}
+	for letter in tc:
+		if letter in ledger:
+			ledger[letter] += 1
+		else:
+			ledger[letter] = 1
+	return ledger
 
 
 main()
